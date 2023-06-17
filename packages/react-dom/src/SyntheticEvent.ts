102efc7a97443a44e7e71f23eb94aa8e7a8856bf
harvey <hyuain97@163.com>
2023-06-17 16:20:35 +0800

    feat: generate priority for event

--- packages/react-dom/src/SyntheticEvent.ts
+++ packages/react-dom/src/SyntheticEvent.ts
@@ -1,5 +1,11 @@
 import { Props } from 'shared/ReactTypes'
 import { Container } from 'hostConfig'
+import {
+  unstable_ImmediatePriority,
+  unstable_NormalPriority,
+  unstable_runWithPriority,
+  unstable_UserBlockingPriority
+} from 'scheduler'
 
 export const elementPropsKey = '__props'
 const validEventTypeList = ['click']
@@ -62,7 +68,10 @@ function dispatchEvent(container: Container, eventType: string, e: Event) {
 function triggerEventFlow(paths: EventCallback[], se: SyntheticEvent) {
   for (let i = 0; i < paths.length; i++) {
     const callback = paths[i]
-    callback.call(null, se)
+    // save the current priority to context
+    unstable_runWithPriority(eventTypeToSchedulerPriority(se.type), () => {
+      callback.call(null, se)
+    })
     if (se.__stopPropagation) {
       break
     }
@@ -117,4 +126,17 @@ function collectPaths(
     targetElement = targetElement.parentNode as DOMElement
   }
   return paths
+}
+
+function eventTypeToSchedulerPriority(eventType: string) {
+  switch (eventType) {
+    case 'click':
+    case 'keydown':
+    case 'keyup':
+      return unstable_ImmediatePriority
+    case 'scroll':
+      return unstable_UserBlockingPriority
+    default:
+      return unstable_NormalPriority
+  }
 }
--- packages/react-reconciler/src/fiberLanes.ts
+++ packages/react-reconciler/src/fiberLanes.ts
@@ -1,8 +1,18 @@
 import { FiberRootNode } from './fiber'
+import {
+  unstable_getCurrentPriorityLevel,
+  unstable_IdlePriority,
+  unstable_ImmediatePriority,
+  unstable_NormalPriority,
+  unstable_UserBlockingPriority
+} from 'scheduler'
 
 export type Lane = number
 export type Lanes = number
 
+export const IdleLane = 0b1000
+export const DefaultLane = 0b0100
+export const InputContinuousLane = 0b0010
 export const SyncLane = 0b0001
 export const NoLane = 0b0000
 export const NoLanes = 0b0000
@@ -12,7 +22,10 @@ export function mergeLanes(a: Lane, b: Lane): Lanes {
 }
 
 export function requestUpdateLane() {
-  return SyncLane
+  // get the schedulerPriority from current context, and convert to lane
+  const currentSchedulerPriority = unstable_getCurrentPriorityLevel()
+  const lane = schedulerPriorityToLane(currentSchedulerPriority)
+  return lane
 }
 
 export function getHighestPriorityLane(lanes: Lanes): Lane {
@@ -22,4 +35,31 @@ export function getHighestPriorityLane(lanes: Lanes): Lane {
 
 export function markRootFinished(root: FiberRootNode, lane: Lane) {
   root.pendingLanes &= ~lane
+}
+
+function lanesToSchedulerPriority(lanes: Lanes) {
+  const lane = getHighestPriorityLane(lanes)
+  if (lane === SyncLane) {
+    return unstable_ImmediatePriority
+  }
+  if (lane === InputContinuousLane) {
+    return unstable_UserBlockingPriority
+  }
+  if (lane === DefaultLane) {
+    return unstable_NormalPriority
+  }
+  return unstable_IdlePriority
+}
+
+function schedulerPriorityToLane(schedulerPriority: number) {
+  if (schedulerPriority === unstable_ImmediatePriority) {
+    return SyncLane
+  }
+  if (schedulerPriority === unstable_UserBlockingPriority) {
+    return InputContinuousLane
+  }
+  if (schedulerPriority === unstable_NormalPriority) {
+    return DefaultLane
+  }
+  return NoLane
 }
